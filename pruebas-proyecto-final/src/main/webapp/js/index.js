70e6edd31c6e5b2df5f5b1f5b8705e6ec41872e6
Lucas <lllencinas@gmail.com>
2015-04-19 16:53:16 -0300

    Agrego unas cosas en el frontend, todavia no se si anda pero lotengo que hacer para hacer el pull

---
+++ pruebas-proyecto-final/src/main/webapp/js/index.js
@@ -0,0 +1,18 @@
+
+function getMyUser(){
+	console.log("A punto de pedir el nombre de un usuario");
+	$.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "truequeLibre/usuarios",
+        success: function (data) {
+        	var items = "";
+        	if(data[0] == null){
+        		$('#espacioNombreUsuario').html("Error!! No esta ese usuario");
+        	}
+        	else{
+        		$('#espacioNombreUsuario').html(data[0]);
+        	}
+        }
+    });
+}
